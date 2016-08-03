8a63e0501f0fbfa031e13ddd0fc64881754fd937
Andor Kelemen <andor.kelemen@gmail.com>
2016-08-03 19:44:42 +0200

    MS-9 Add initial config service

---
+++ src/main/java/com/msteam/NutaxiConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.msteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutaxiConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutaxiConfigServiceApplication.class, args);
+	}
+}
