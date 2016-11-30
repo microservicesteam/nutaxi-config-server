75966373bbbce246934afaf72a65cc08810d21bf
Balázs Szögi <szogibalu@gmail.com>
2016-11-30 19:09:33 +0100

    Update NutaxiConfigServiceApplication.java

--- src/main/java/com/msteam/NutaxiConfigServiceApplication.java
+++ src/main/java/com/msteam/NutaxiConfigServiceApplication.java
@@ -7,12 +7,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class NutaxiConfigServiceApplication {
-    
-    public String fixMeLater() {
-        return "I'm here only so Checkstyle and PMD are not raging on utility class rules";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(NutaxiConfigServiceApplication.class, args);
     }
+    
 }
